c2d6a32dfe04b669587be84d93bac24795fb5dae
Raghunath.Venkata <sv_raghunath@hotmail.com>
2020-08-03 20:41:59 +0100

    fifth commit

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -3,12 +3,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-    username: {
         username: { type: String, required: true },
         description: {type: String, required: true},
         duration: {type: Number, required: true},
         date: {type: Date, required: true}
-    }
+    
 }, {
     timestamps: true
 })
--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Exercise from '../models/user.model'
+import Exercise from '../models/exercise.model'
 
 const router = express.Router()
 
@@ -24,5 +24,36 @@ router.route('/add').post((req, res) => {
     newExercise.save().then(() => res.json('Exercise added'))
     .catch(error => res.status(400).json('Error: ' + error))
 })
+
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+  .then(exercises => res.json(exercises))
+  .catch(err => res.status(400).json('Error ' + error))  
+})
+
+
+router.route('/delete/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('exercise deleted'))
+    .catch(err => res.status(400).json('Error ' + error))  
+  })
+
+
+  router.route('/update/:id').put((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercises => {
+        exercises.username = req.body.username
+        exercises.description = req.body.description
+        exercises.duration = Number(req.body.duration)
+        exercises.date = Date.parse(req.body.date)
+
+        exercises.save().then(() => res.json('Exercise updated'))
+        .catch(error => res.status(400).json('Error: ' + error))
+
+    })
+    .catch(err => res.status(400).json('Error ' + error))  
+  })
+
+
 
 export default router
