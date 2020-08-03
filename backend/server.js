3730a6ad414d94fe65e6439be6213b1cc3702635
Raghunath.Venkata <sv_raghunath@hotmail.com>
2020-08-03 14:16:07 +0100

    second commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyparser from "body-parser";
+import cors from "cors";
+import dotenv from 'dotenv'
+
+const x = dotenv.config()
+const app = express();
+const PORT = 4000;
+
+//body parser setup
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+
+const uri = process.env.ATLAS_URI
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  uri,
+  {
+    user: "soccerapp",
+    pass: "soccerapp",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('DB connection succesful!!!')
+})
+
+app.listen(PORT, () => { console.log(`Server started at ${PORT}`) });
+app.get("/", (req, res) => { res.send(`App is running on ${PORT}`) });
+app.get("/hello", (req, res) => { setTimeout(() => { res.send(`Hello there from ${process.env.PORT}!`) }, 2360) });
